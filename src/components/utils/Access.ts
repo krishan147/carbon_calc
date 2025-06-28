1ac906b68037624fc6ea507c696b667d152d7c27
Krishan Vadher <krishan.vadher@gmail.com>
2025-06-28 21:52:16 +0100

    test-push

--- src/components/utils/Access.ts
+++ src/components/utils/Access.ts
@@ -1,26 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-import { fetchAuthSession } from 'aws-amplify/auth';
 
+export async function get_table(search: String) {
 
-export async function get_token() {
-
-  const session = await fetchAuthSession({ forceRefresh: true }); 
-  const idToken = session.tokens.idToken.toString();
-
-  return { idToken };
-}
-
-
-export async function get_table(search) {
-
-  const { idToken } = await get_token()
   
   const url = 'https://sidwswpe72.execute-api.eu-west-1.amazonaws.com/production/todayssums';
 
   const headers = {
-      'Content-Type': 'application/json',
-      "Authorization": `Bearer ${idToken}`
+      'Content-Type': 'application/json'
   };
 
   try {
@@ -38,6 +25,5 @@ export async function get_table(search) {
       return data;
   } catch (error) {
       console.error('Error:', error);
-      throw error;
   } 
 }
